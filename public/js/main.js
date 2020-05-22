ced2ed112312d85071784196bd7c7c592e9d815b
Gaetano Barreca <gaetanobarreca@gmail.com>
2020-05-22 18:31:01 +0100

    added Sprites & Layers

--- public/js/main.js
+++ public/js/main.js
@@ -1,30 +1,43 @@
-import SpriteSheet from './SpriteSheet.js';
-import { loadImage, loadLevel } from './loaders.js';
+import Compositor from './Compositor.js/index.js';
+import { loadLevel } from './loaders.js';
+import { loadMarioSprite, loadBackgroundSprites } from './sprites.js';
+import { createBackgroundLayer } from './layers.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-function drawBackground(background, context, sprites) {
-    background.ranges.forEach(([x1, x2, y1, y2]) => {
-        for (let x = x1; x < x2; ++x) {
-            for (let y = y1; y < y2; ++y) {
-                sprites.drawTile(background.tile, context, x, y);
-            }
-        }
-    });
+
+function createSpriteLayer(sprite, pos) {
+    return function drawSpriteLayer(context) {
+        sprite.draw('idle', context, pos.x, pos.y);
+    };
 }
 
 
-loadImage('/img/tiles.png')
-    .then(image => {
-        const sprites = new SpriteSheet(image);
-        sprites.define('ground', 0, 0);
-        sprites.define('sky', 3, 23);
+Promise.all([
+    loadMarioSprite(),
+    loadBackgroundSprites(),
+    loadLevel('1-1'),
+])
+    .then(([marioSprite, backgroundSprites, level]) => {
+        console.log('Level loader', level);
 
-        loadLevel('1-1')
-            .then(level => {
-                level.backgrounds.forEach(background => {
-                    drawBackground(background, context, sprites);
-                });
-            });
+        const comp = new Compositor();
+        comp.layers.push(createBackgroundLayer(level.backgrounds, backgroundSprites));
+
+        const pos = {
+            x: 64,
+            y: 64,
+        };
+
+        comp.layers.push(createSpriteLayer(marioSprite, pos));
+
+        function update() {
+            comp.draw(context);
+            pos.x += 2;
+            pos.y += 1;
+            requestAnimationFrame(update);
+        }
+
+        update();
     });
